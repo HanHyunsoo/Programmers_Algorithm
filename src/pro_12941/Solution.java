21be5dcb7a024ac5e897b27d27d132a1ca648abc
HanHyunSoo <gustn4563@gmail.com>
2022-06-19 13:30:49 +0900

    최솟값 만들기

---
+++ src/pro_12941/Solution.java
@@ -0,0 +1,19 @@
+package pro_12941;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - (1 + i)];
+        }
+
+        return answer;
+    }
+}
